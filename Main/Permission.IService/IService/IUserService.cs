9cb57ce9927da53c994473a4039d529349bd496d
阿冲 <1253627899@qq.com>
2026-09-14 21:47:23 +0800

    init

---
+++ Main/Permission.IService/IService/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Permission.IService.IService
+{
+    public interface IUserService
+    {
+        public List<userModel> GetUsers();
+        public bool Delete();
+        public bool Edit(List<string> users);
+
+
+    }
+}
